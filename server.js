d65951d381ea699cae0d9bd813858f7524ed618d
FlokiJaws <95760524+FlokiJaws@users.noreply.github.com>
2025-05-14 10:42:41 +0200

    fd

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ const reviewNotificationsRoutes = require("./routes/reviewNotifications");
 const userNotificationsRoutes = require("./routes/userNotifications");
 const verificationRoutes = require("./routes/verificationRoutes");
 const registrationRoutes = require("./routes/registrationRoutes");
-const preRegistrationRoutes = require("./routes/preRegistrationRoutes");
 // Importer d'autres routes au besoin
 // Importer d'autres routes au besoin
 
